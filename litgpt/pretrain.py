6439862f6ad621ce363382d1710551a446d74771
Erik Sik <eriksik97@gmail.com>
2026-09-07 19:28:54 +0200

    Wire gated self-state into GPT, config, and pretrain loss

--- litgpt/pretrain.py
+++ litgpt/pretrain.py
@@ -1,1 +1,45 @@
-PLACEHOLDER_REPLACE_WITH_FILE
+# Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
+
+import math
+import pprint
+import time
+import warnings
+from dataclasses import asdict
+from datetime import timedelta
+from functools import partial
+from pathlib import Path
+from typing import Literal
+
+import lightning as L
+import torch
+import torch.nn as nn
+from lightning.fabric.strategies import FSDPStrategy
+from lightning.fabric.utilities.throughput import ThroughputMonitor, measure_flops
+from torch.utils.data import DataLoader
+from torchmetrics.aggregation import RunningMean
+
+from litgpt import Tokenizer
+from litgpt.args import EvalArgs, LogArgs, TrainArgs
+from litgpt.config import name_to_config
+from litgpt.constants import _TORCH_EQUAL_2_7, _TORCH_EQUAL_2_8
+from litgpt.data import DataModule, TinyLlama
+from litgpt.model import GPT, Block, CausalSelfAttention, Config, LLaMAMLP, SelfStateCell
+from litgpt.parser_config import save_hyperparameters
+from litgpt.types import LoggerChoice
+from litgpt.utils import (
+    CycleIterator,
+    capture_hparams,
+    check_nvlink_connectivity,
+    choose_logger,
+    chunked_cross_entropy,
+    copy_config_files,
+    extend_checkpoint_dir,
+    find_resume_path,
+    get_default_supported_precision,
+    init_out_dir,
+    instantiate_torch_optimizer,
+    num_parameters,
+    parse_devices,
+    reset_parameters,
+    save_config,
+)
